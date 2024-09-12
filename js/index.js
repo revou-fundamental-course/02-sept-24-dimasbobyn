84d02737c206d0976344b169160d3f209cf4d635
dimasbobyn <57338705+dimasbobyn@users.noreply.github.com>
2024-09-12 22:53:39 +0700

    rumus & tampilan

--- js/index.js
+++ js/index.js
@@ -1,23 +1,80 @@
+// Menampilkan Rumus Segitiga
+function pilihRumusSegitiga(){
+    var tampilLuasSegitiga = document.getElementById('luas-segitiga');
+    var tampilKelilingSegitiga =document.getElementById('keliling-segitiga');
+    var tampilLuasJajarGenjang = document.getElementById('luas-jajar-genjang');
+    var tampilKelilingJajarGenjang =document.getElementById('keliling-jajar-genjang');
 
+    tampilLuasSegitiga.style.display = 'block';
+    tampilKelilingSegitiga.style.display ='block';
+    tampilLuasJajarGenjang.style.display = 'none';
+    tampilKelilingJajarGenjang.style.display ='none';
+}
 
+// Menampilkan Rumus Jajar Genjang
+function pilihRumusGagargenjang(){
+    var tampilLuasSegitiga = document.getElementById('luas-segitiga');
+    var tampilKelilingSegitiga =document.getElementById('keliling-segitiga');
+    var tampilLuasJajarGenjang = document.getElementById('luas-jajar-genjang');
+    var tampilKelilingJajarGenjang =document.getElementById('keliling-jajar-genjang');
 
+    tampilLuasSegitiga.style.display = 'none';
+    tampilKelilingSegitiga.style.display ='none';
+    tampilLuasJajarGenjang.style.display = 'block';
+    tampilKelilingJajarGenjang.style.display ='block';
+}
 
+// Rumus Luas Segitiga
 function luasSegitiga() {
-    let panjang = 5;
-    let alas = 3;
-    hasil = panjang * alas / 2;
-    document.getElementById("hasil-luas").innerHTML="1/2 X " + panjang + " X " + alas + " = " + hasil;
+    var alas = parseFloat(document.getElementById("alas-segitiga").value);
+    var tinggi = parseFloat(document.getElementById("tinggi-segitiga").value);
+    var hasilLuas = document.getElementById("hasil-luas-segitiga");
+    var luas = 0.5 * alas * tinggi;
+    hasilLuas.innerHTML="Luas = 1/2 X " +alas+ " X " +tinggi+ "<br>Luas = " +luas;
 }
 
-
+// Rumus Keliling Segitiga
 function kelilingSegitiga() {
-    let sisiab = 3;
-    let sisibc = 3;
-    let sisica = 3;
-    hasil = sisiab+sisibc+sisica;
-    document.getElementById("hasil-keliling").innerHTML= sisiab + " + " + sisibc + " + " + sisica + " = "  +hasil;
+    var sisiAb = parseFloat(document.getElementById("sisi-ab").value);
+    var sisiBc = parseFloat(document.getElementById("sisi-bc").value);
+    var sisiCa = parseFloat(document.getElementById("sisi-ca").value);
+    var hasilKeliling = document.getElementById("hasil-keliling-segitiga");
+    var keliling = sisiAb + sisiBc + sisiCa
+    hasilKeliling.innerHTML="Keliling = " +sisiAb+ " + " +sisiBc+ " + " +sisiCa+ "<br>Keliling = " +keliling;
 }
 
-function resetLuasSegitiga(){
-    document.getElementById("hasil-luas").innerHTML="";
+// Rumus Luas Jajar Genjang
+function luasJajarGenjang() {
+    var alas = parseFloat(document.getElementById("alas").value);
+    var tinggi = parseFloat(document.getElementById("tinggi").value);
+    var hasilLuas = document.getElementById("hasil-luas-jajar-genjang");
+    var luas = alas * tinggi;
+    hasilLuas.innerHTML="Luas = " +alas+ " X " +tinggi+ "<br>Luas = " +luas;
+}
+
+// Rumus Keliling Jajar Genjang
+function kelilingJajarGenjang() {
+    var sisiAlas = parseFloat(document.getElementById("sisi-alas").value);
+    var sisiMiring = parseFloat(document.getElementById("sisi-miring").value);
+    var hasilKeliling = document.getElementById("hasil-keliling-jajar-genjang");
+    var keliling = 2 * (sisiAlas + sisiMiring);
+    hasilKeliling.innerHTML="Keliling = (" +sisiAlas+ " + " +sisiMiring+ ") X 2" +"<br>Keliling = " +keliling;
+}
+
+
+
+
+
+// Reset Hasil
+function resetLuasSegigtiga(){
+    document.getElementById("hasil-luas-segitiga").innerHTML="";
+}
+function resetKelilingSegitiga(){
+    document.getElementById("hasil-keliling-segitiga").innerHTML="";
+}
+function resetLuasJajarGenjang(){
+    document.getElementById("hasil-luas-jajar-genjang").innerHTML="";
+}
+function resetKelilingJajarGenjang(){
+    document.getElementById("hasil-keliling-jajar-genjang").innerHTML="";
 }
