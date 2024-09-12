8a343ac57794d1e4f61bf436f1378d559f1893d6
dimasbobyn <57338705+dimasbobyn@users.noreply.github.com>
2024-09-12 17:34:35 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
+
+
+
+
 function luasSegitiga() {
-    var panjang = 5;
-    var alas = 3;
+    let panjang = 5;
+    let alas = 3;
     hasil = panjang * alas / 2;
     document.getElementById("hasil-luas").innerHTML="1/2 X " + panjang + " X " + alas + " = " + hasil;
 }
 
 
 function kelilingSegitiga() {
-    var sisiab = 3;
-    var sisibc = 3;
-    var sisica = 3;
+    let sisiab = 3;
+    let sisibc = 3;
+    let sisica = 3;
     hasil = sisiab+sisibc+sisica;
     document.getElementById("hasil-keliling").innerHTML= sisiab + " + " + sisibc + " + " + sisica + " = "  +hasil;
+}
+
+function resetLuasSegitiga(){
+    document.getElementById("hasil-luas").innerHTML="";
 }
